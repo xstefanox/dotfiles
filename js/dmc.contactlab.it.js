d06ca329f11c035f5a6d3b1d72a167ef85447c29
Stefano Varesi <stefano.varesi@gmail.com>
2013-09-06 12:40:29 +0200

    Added custom js and css

---
+++ js/dmc.contactlab.it.js
@@ -0,0 +1,10 @@
+// autologin
+$(function()
+{
+    if (/^\/login/.test(window.location.pathname) && window.contactlab)
+    {
+        $('input[name=username]').val(window.contactlab.username);
+        $('input[type=password]').val(window.contactlab.password);
+        $('input[type=submit]').trigger('click');
+    }
+});
