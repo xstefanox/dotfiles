b418857d739e41dcb2d9676ec9c5a4283e211118
Stefano Varesi <stefano.varesi@gmail.com>
2015-03-11 12:07:32 +0100

    Added more APT repositories

--- apt.sources.list.sh
+++ apt.sources.list.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0]]
+then
+    echo "You must be root"
+    exit 1
+fi
+
 # atom
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 add-apt-repository ppa:webupd8team/atom
@@ -13,9 +19,21 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 91E7EE5E
 add-apt-repository ppa:team-xbmc/ppa
 
 # spotify
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 echo "deb http://repository.spotify.com stable non-free" > /etc/apt/sources.list.d/spotify.list
 
 # heroku
 wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
 echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+
+# nodejs
+wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo 'deb https://deb.nodesource.com/node trusty main' > /etc/apt/sources.list.d/nodejs.list
+
+# vagrant (unofficial)
+apt-key adv --keyserver pgp.mit.edu --recv-key 2099F7A4
+echo 'deb http://vagrant-deb.linestarve.com/ any main' > /etc/apt/sources.list.d/vagrant.list
+
+# git
+#apt-key adv --keyserver keyserver.ubuntu.com --recv-key E1DF1F24
+add-apt-repository ppa:git-core/ppa
