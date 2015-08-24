8555112b41d6ce28c47543409ca6a1673c6f68a0
Stefano Varesi <stefano.varesi@gmail.com>
2015-08-24 20:45:16 +0200

    Git configuration refactoring

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -15,7 +15,6 @@ then
         git config --global credential.helper cache
     fi
 
-    git config --global color.ui true
     git config --global color.status.added     "green $([[ $OSTYPE == linux* || $OSTYPE == cygwin ]] && echo bold)"
     git config --global color.status.changed   "yellow $([[ $OSTYPE == linux* || $OSTYPE == cygwin ]] && echo bold)"
     git config --global color.status.untracked "red $([[ $OSTYPE == linux* || $OSTYPE == cygwin ]] && echo bold)"
@@ -27,16 +26,4 @@ then
     git config --global color.branch.current  "green $([[ $OSTYPE == linux* || $OSTYPE == cygwin ]] && echo bold)"
     git config --global color.branch.remote   "red $([[ $OSTYPE == linux* || $OSTYPE == cygwin ]] && echo bold)"
 
-    git config --global core.excludesfile "~/.gitignore.global"
-    git config --global push.default $(git --version | grep --silent " 1.8" && echo simple || echo matching)
-
-    # @see https://gist.github.com/unphased/5303697
-    git config --global alias.plog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%an <%ae>%Creset' --abbrev-commit --date=relative"
-    git config --global alias.s status
-
-    git config --global tag.sort version:refname
-
-    git config --global pager.log  'diff-highlight | less'
-    git config --global pager.show 'diff-highlight | less'
-    git config --global pager.diff 'diff-highlight | less'
 fi
