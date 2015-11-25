ef3abf2ac8d3645877cb70759068377cba2aca9d
Stefano Varesi <stefano.varesi@contactlab.com>
2015-11-25 17:16:36 +0100

    Added NGINX PPA

--- apt.sources.list.sh
+++ apt.sources.list.sh
@@ -56,3 +56,8 @@ echo 'deb http://repo.mysql.com/apt/ubuntu/ vivid workbench-6.2' >> /etc/apt/sou
 
 # gpu drivers
 add-apt-repository ppa:graphics-drivers/ppa
+
+# nginx
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+
