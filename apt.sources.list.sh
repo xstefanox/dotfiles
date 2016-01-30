a7054d7b63abab72687cf13a52a91459638f21e2
Stefano Varesi <stefano.varesi@gmail.com>
2016-01-30 17:59:20 +0100

    added mongodb APT sources

--- apt.sources.list.sh
+++ apt.sources.list.sh
@@ -67,3 +67,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 
 # geary
 add-apt-repository ppa:yorba/ppa
+
+# mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
