36da824e5d0c55d51ad7cfe33b5cb485eebe005d
Stefano Varesi <stefano.varesi@gmail.com>
2016-12-08 21:24:48 +0100

    optimize homebrew and npm scripts

--- bashrc.d/homebrew.sh
+++ bashrc.d/homebrew.sh
@@ -11,5 +11,6 @@ then
     export PATH=$HOME/Library/Homebrew/sbin:$PATH
 
     # PHP 5.6 support
-    export PATH="$(brew --prefix homebrew/php/php56)/bin:$PATH"
+    [[ ! -f "${HOME}/.brew-prefix-php56" ]] && brew --prefix homebrew/php/php56 > "${HOME}/.brew-prefix-php56"
+    export PATH="$(< ${HOME}/.brew-prefix-php56)/bin:$PATH"
 fi
--- bashrc.d/nodejs.sh
+++ bashrc.d/nodejs.sh
@@ -9,9 +9,11 @@
 if which npm &> /dev/null
 then
     # set the correct paths to install global npm modules in user home
-    npm config set prefix ~/.npm/packages
-    npm config set cache ~/.npm/cache
-    npm config set progress false
+    npm_config="$(< ${HOME}/.npmrc)"
+    echo "$npm_config" | grep -q "prefix=${HOME}/.npm/packages" || npm config set prefix ~/.npm/packages
+    echo "$npm_config" | grep -q "cache=${HOME}/.npm/cache" || npm config set cache ~/.npm/cache
+    echo "$npm_config" | grep -q "progress=false" || npm config set progress false
+    unset npm_config
 
     # add the npm binary path to the PATH
     PATH=$HOME/.npm/packages/bin:$PATH
