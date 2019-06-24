ea6055aa2599cb6cb8fa20122ea82b6bdb1e0735
Stefano Varesi <stefano.varesi@gmail.com>
2019-06-24 14:42:08 +0200

    removed explicit PHP 5.6 support from homebrew

--- bashrc.d/homebrew.sh
+++ bashrc.d/homebrew.sh
@@ -9,8 +9,4 @@ if [[ $OSTYPE == darwin* && -d "$HOME/Library/Homebrew" ]]
 then
     export PATH=$HOME/Library/Homebrew/bin:$PATH
     export PATH=$HOME/Library/Homebrew/sbin:$PATH
-
-    # PHP 5.6 support
-    [[ ! -f "${HOME}/.brew-prefix-php56" ]] && brew --prefix homebrew/php/php56 > "${HOME}/.brew-prefix-php56"
-    export PATH="$(< ${HOME}/.brew-prefix-php56)/bin:$PATH"
 fi
