5edbfaf63338c8df17668deff6a58694d360942a
Stefano Varesi <stefano.varesi@gmail.com>
2020-08-10 09:41:45 +0200

    ruby loading cleanup

--- bashrc.d/ruby.sh
+++ bashrc.d/ruby.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#########
+## RVM ##
+#########
+
+source "${HOME}/.rvm/scripts/rvm"
+
 ##########
 ## RUBY ##
 ##########
@@ -8,14 +14,4 @@
 if which gem &> /dev/null
 then
     export PATH="$(gem env | sed -n '/GEM PATHS/,/GEM/ p' | grep "${HOME}" | sed -e "s:.*\(${HOME}.*\):\1:" | paste -s -d':' -)/bin:${PATH}"
-fi
-
-#########
-## RVM ##
-#########
-
-if [[ -d "${HOME}/.rvm" ]]
-then
-    PATH=$HOME/.rvm/bin:$PATH
-    [[ -s "${HOME}/.rvm/scripts/rvm" ]] && source "${HOME}/.rvm/scripts/rvm"
 fi
