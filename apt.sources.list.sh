cb5ae54607d4bd4e04b55b01a4e187b46b3184f2
Stefano Varesi <stefano.varesi@gmail.com>
2021-02-14 18:33:16 +0100

    added terraform apt repository

--- apt.sources.list.sh
+++ apt.sources.list.sh
@@ -49,18 +49,17 @@ apt-add-repository ppa:ansible/ansible
 apt-add-repository ppa:ubuntu-wine/ppa
 
 # mysql workbench
-echo 'deb http://repo.mysql.com/apt/ubuntu/ precise workbench-6.2' > /etc/apt/sources.list.d/mysql-workbench.list
-echo 'deb http://repo.mysql.com/apt/ubuntu/ vivid workbench-6.2' >> /etc/apt/sources.list.d/mysql-workbench.list
+echo 'deb http://repo.mysql.com/apt/ubuntu/ focal workbench-6.2' >> /etc/apt/sources.list.d/mysql-workbench.list
 
 # gpu drivers
 add-apt-repository ppa:graphics-drivers/ppa
 
 # nginx
-echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/nginx-stable.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
 
 # google chrome
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+echo 'deb [arch=$(dpkg --print-architecture)] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 # geary
@@ -73,4 +72,8 @@ echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" 
 
 # visual studio code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+echo "deb [arch=$(dpkg --print-architecture)] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+# terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com focal main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
