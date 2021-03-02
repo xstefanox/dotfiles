8de70f93b836bad2df076fbfcbf2c7b83ef2ff75
Stefano Varesi <stefano.varesi@gmail.com>
2021-03-02 09:56:31 +0100

    added some useful aliases for docker

--- bashrc.d/docker.sh
+++ bashrc.d/docker.sh
@@ -6,3 +6,5 @@
 
 alias docker-purge-images='docker rmi $(docker images -q)'
 alias docker-purge-containers='docker rm $(docker ps -a -q)'
+alias docker-purge-volumes='docker volume ls --format {{.Name}} | xargs docker volume rm'
+alias docker-purge-testcontainers='docker ps -a --format {{.ID}} --filter label=org.testcontainers=true | xargs docker rm -f'
