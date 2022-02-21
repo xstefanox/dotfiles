492bb533d91de81e3d1fa3aeac1d78aa2388a054
Stefano Varesi <stefano.varesi@gmail.com>
2022-02-21 11:53:01 +0100

    better docker alias to purge containers

--- bashrc.d/docker.sh
+++ bashrc.d/docker.sh
@@ -5,6 +5,7 @@
 ############
 
 alias docker-purge-images='docker rmi $(docker images -q)'
-alias docker-purge-containers='docker rm $(docker ps -a -q)'
+alias docker-purge-containers='docker ps --all --format {{.ID}} | xargs docker rm --force'
 alias docker-purge-volumes='docker volume ls --format {{.Name}} | xargs docker volume rm'
-alias docker-purge-testcontainers='docker ps -a --format {{.ID}} --filter label=org.testcontainers=true | xargs docker rm -f'
+alias docker-purge-tc-containers='docker ps --all --format {{.ID}} --filter label=org.testcontainers=true | xargs docker rm --force --volumes'
+alias docker-purge-tc-networks='docker network ls --format {{.ID}} --filter label=org.testcontainers=true | xargs docker network rm'
