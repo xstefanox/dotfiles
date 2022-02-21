f88d9b28efcd4af48afee94f21ae890169469564
Stefano Varesi <stefano.varesi@gmail.com>
2022-02-21 14:29:39 +0100

    added some Hombrew configurations

--- env.d/brew.sh
+++ env.d/brew.sh
@@ -1,12 +1,14 @@
 if [[ $OSTYPE == darwin* ]]
 then
 
-    BREW_PREFIX=$HOME/.local/opt/homebrew
+    export BREW_PREFIX=$HOME/.local/opt/homebrew
 
     if [[ -d "$BREW_PREFIX" ]]
     then
         export PATH=$BREW_PREFIX/bin:$PATH
         export PATH=$BREW_PREFIX/sbin:$PATH
+        export HOMEBREW_NO_ENV_HINTS=true
+        export HOMEBREW_NO_AUTO_UPDATE=true
     else
         unset BREW_PREFIX
     fi
