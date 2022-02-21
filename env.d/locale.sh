eaf8d19560cc2959a3effb63697cc85fadf155a1
Stefano Varesi <stefano.varesi@gmail.com>
2022-02-21 14:31:03 +0100

    force default it lang

--- env.d/locale.sh
+++ env.d/locale.sh
@@ -4,6 +4,5 @@
 ## LOCALIZATION ##
 ##################
 
-[[ -z "$LC_ALL" && -n "$LANG" ]] && export LC_ALL="$LANG"
-[[ -z "$LANG" && -n "$LC_ALL" ]] && export LANG="$LC_ALL"
-[[ -z "$LC_CTYPE" && -n "$LC_ALL" ]] && export LC_CTYPE="$LC_ALL"
+export LANG=it_IT.UTF-8
+export LC_ALL=it_IT.UTF-8
