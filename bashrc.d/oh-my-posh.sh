31afdd40fd7c0dd0b0357b16c781ca0ba00b570d
Stefano Varesi <stefano.varesi@gmail.com>
2022-04-08 21:09:53 +0200

    fix: load oh-my-posh only if installed

--- bashrc.d/oh-my-posh.sh
+++ bashrc.d/oh-my-posh.sh
@@ -1,1 +1,4 @@
-eval "$(oh-my-posh --init --shell bash --config ${HOME}/.oh-my-posh.json)"
+if oh-my-posh &> /dev/null
+then
+    eval "$(oh-my-posh --init --shell bash --config ${HOME}/.oh-my-posh.json)"
+fi
