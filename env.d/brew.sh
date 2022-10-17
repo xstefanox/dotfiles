c1be494a42f181f1e290946d93cdccacf97c11e7
Stefano Varesi <stefano.varesi@gmail.com>
2022-10-17 19:04:36 +0200

    feat: use Hombrew default path, some additional configuration set

--- env.d/brew.sh
+++ env.d/brew.sh
@@ -1,15 +1,7 @@
 if [[ $OSTYPE == darwin* ]]
 then
-    export BREW_PREFIX=$HOME/.local/opt/homebrew
-
-    if [[ -d "$BREW_PREFIX" ]]
-    then
-        export PATH=$BREW_PREFIX/bin:$PATH
-        export PATH=$BREW_PREFIX/sbin:$PATH
-        export HOMEBREW_NO_ENV_HINTS=true
-        export HOMEBREW_NO_AUTO_UPDATE=true
-        export HOMEBREW_CASK_OPTS="--no-quarantine"
-    else
-        unset BREW_PREFIX
-    fi
+    export BREW_PREFIX=/usr/local
+    export HOMEBREW_NO_ENV_HINTS=true
+    export HOMEBREW_NO_AUTO_UPDATE=true
+    export HOMEBREW_CASK_OPTS="--no-quarantine"
 fi
