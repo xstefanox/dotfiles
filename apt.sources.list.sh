0e5c89d204d7e1a280c1701604e3d9deb73ad680
Stefano Varesi <stefano.varesi@gmail.com>
2023-02-05 14:55:07 +0100

    fix: Spotify APT key

--- apt.sources.list.sh
+++ apt.sources.list.sh
@@ -12,7 +12,7 @@ UBUNTU_BASE_VERSION=kinetic
 add-apt-repository ppa:team-xbmc/ppa
 
 # spotify
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/spotify.gpg
 echo "deb [signed-by=/etc/apt/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # heroku
