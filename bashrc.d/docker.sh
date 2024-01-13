ab33f5db6fb4e32d795a3f00adf7843aafefcd05
Stefano Varesi <stefano.varesi@gmail.com>
2024-01-13 10:57:12 +0100

    feature: additional Docker aliases

--- bashrc.d/docker.sh
+++ bashrc.d/docker.sh
@@ -9,3 +9,4 @@ alias docker-purge-containers='docker ps --all --format {{.ID}} | xargs docker r
 alias docker-purge-volumes='docker volume ls --format {{.Name}} | xargs docker volume rm'
 alias docker-purge-tc-containers='docker ps --all --format {{.ID}} --filter label=org.testcontainers=true | xargs docker rm --force --volumes'
 alias docker-purge-tc-networks='docker network ls --format {{.ID}} --filter label=org.testcontainers=true | xargs docker network rm'
+alias docker-purge-stopped-containers='docker ps --all --format {{.ID}} --filter "status=exited" | xargs docker rm --force'
