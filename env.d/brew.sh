89023f8b89bdfa10dc47a8e43152fdbce3714352
Stefano Varesi <stefano.varesi@gmail.com>
2024-08-05 17:12:30 +0200

    feat: new integration for Homebrew

--- env.d/brew.sh
+++ env.d/brew.sh
@@ -1,6 +1,14 @@
 if [[ $OSTYPE == darwin* ]]
 then
-    export BREW_PREFIX=/usr/local
+    export HOMEBREW_PREFIX="/opt/homebrew";
+    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+    export HOMEBREW_REPOSITORY="/opt/homebrew";
+    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+    if [ ! -z "${MANPATH-}" ]
+    then
+        export MANPATH=":${MANPATH#:}";
+    fi
     export HOMEBREW_NO_ENV_HINTS=true
     export HOMEBREW_NO_AUTO_UPDATE=true
     export HOMEBREW_CASK_OPTS="--no-quarantine"
