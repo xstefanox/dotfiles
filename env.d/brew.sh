402aa740deb3c2e2c12e1d703ff38209d7c26c21
Stefano Varesi <stefano.varesi@gmail.com>
2025-09-23 17:23:34 +0200

    feat: custom installation path for Homebrew on macOS

--- env.d/brew.sh
+++ env.d/brew.sh
@@ -1,15 +1,26 @@
 if [[ $OSTYPE == darwin* ]]
 then
-    export HOMEBREW_PREFIX="/opt/homebrew";
-    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-    export HOMEBREW_REPOSITORY="/opt/homebrew";
-    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
-    if [ ! -z "${MANPATH-}" ]
+    LOCAL_HOMEBREW_PATH=${HOME}/.local/opt/homebrew
+
+    if [[ -d ${LOCAL_HOMEBREW_PATH} ]]
     then
-        export MANPATH=":${MANPATH#:}";
+        export PATH="${LOCAL_HOMEBREW_PATH}/bin:${LOCAL_HOMEBREW_PATH}/sbin${PATH+:$PATH}"
+        export INFOPATH="${LOCAL_HOMEBREW_PATH}/share/info:${INFOPATH:-}";
     fi
-    export HOMEBREW_NO_ENV_HINTS=true
-    export HOMEBREW_NO_AUTO_UPDATE=true
-    export HOMEBREW_CASK_OPTS="--no-quarantine"
+
+    if which brew &> /dev/null
+    then
+        eval "$(brew shellenv)"
+
+        if [ ! -z "${MANPATH-}" ]
+        then
+            export MANPATH=":${MANPATH#:}";
+        fi
+
+        export HOMEBREW_NO_ENV_HINTS=true
+        export HOMEBREW_NO_AUTO_UPDATE=true
+        export HOMEBREW_CASK_OPTS="--no-quarantine --appdir=${HOME}/Applications --fontdir=${HOME}/Library/Fonts"
+    fi
+
+    unset LOCAL_HOMEBREW_PATH
 fi
